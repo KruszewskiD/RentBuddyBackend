51e539414cdb63e3aab67706cc3037318700fea5
Daniel Kruszewski <danielkruszewski@mbp-daniel.home>
2024-07-27 23:14:41 +0200

    Create UserService with createUser method

---
+++ services/UserService.js
@@ -0,0 +1,27 @@
+const User = require("../models/User")
+
+class UserService{
+    static async createUser(first_name, last_name, email, username, password, role="user"){
+        try{
+            if (!firstName || !lastName || !email || !username || !password || !role) {
+                throw new Error("All fields are required");
+            }
+
+            const existingUserByEmail = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+            if (existingUserByEmail.rows.length > 0) {
+                throw new Error("Email already in use");
+            }
+
+            const existingUserByUsername = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+            if (existingUserByUsername.rows.length > 0) {
+                throw new Error("Username already in use");
+            }
+
+            const newUser = await User.create(first_name, last_name, email, username, password, role)
+            
+            return newUser
+        }catch(err){
+            throw new Error(`Error creating user: ${error.message}`);
+        }
+    }
+}
