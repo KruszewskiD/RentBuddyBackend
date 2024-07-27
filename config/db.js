d4415893a6d9fc7d24faccadac1a6838b03463b8
Daniel Kruszewski <danielkruszewski@mbp-daniel.home>
2024-07-27 19:38:37 +0200

    Installed dotenv package

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DV_PORT, 
+});
+
+module.exports = pool;
---
+++ services/InvoiceSerivce.js
@@ -0,0 +1,15 @@
+class InvoiceSerivce{
+    static createInvoice(){
+
+    }
+    static findInvoiceByUserId(){
+        //TODO: sentInvoices
+        //TODO: recivedInvoices
+    }
+    static sendInvoice(){
+        // TODO: Should send invoice from creator to reciver on email?
+    }
+    static updateInvoice(){
+        //TODO: Update invoice data in DB
+    }
+}
