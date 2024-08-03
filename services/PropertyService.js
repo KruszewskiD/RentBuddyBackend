4354e12c8191c0b9ef2c9dcb979d9c6a5ccb33d5
KruszewskiD <daniel.patryk.kruszewski@gmail.com>
2024-08-03 23:29:25 +0200

    Created: PropertService.createProperty

---
+++ services/PropertyService.js
@@ -0,0 +1,24 @@
+const Property = require("../models/Property")
+
+class PropertyService{
+    static async createProperty(address, owner_id, tenant_id=null){
+        try{
+            if (address, owner_id) {
+                throw new Error("Address and owner_id are required");
+            }
+
+            const existingPropertyByAddress = await pool.query('SELECT * FROM properties WHERE address = $1', [address]);
+            if (existingPropertyByAddress.rows.length > 0) {
+                throw new Error("Address already in use");
+            }
+
+            const newProperty = await Property.create(address, owner_id, tenant_id)
+            
+            return newProperty
+        }catch(err){
+            throw new Error(`Error creating property: ${error.message}`);
+        }
+    }
+}
+
+module.exports = PropertyService;
